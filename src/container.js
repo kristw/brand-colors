53368b42db34e97960bcd717b5f0ec999ad27326
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-03-03 16:41:38 -0800

    add google analytics for interactions

--- src/container.js
+++ src/container.js
@@ -1,3 +1,5 @@
+/* global ga:true */
+
 import Actions from './actions';
 import Component from './App';
 import { connect } from 'react-redux';
@@ -29,17 +31,25 @@ function mapDispatchToProps(dispatch) {
   return {
     onInit() {
       dispatch(Actions.buildBoard());
+      ga('send', 'event', 'build_board', 'init');
     },
     onCellClick(index, cell) {
       if (!cell.open) {
         dispatch(Actions.flip(index));
+        ga('send', 'event', 'flip', 'box');
       }
     },
-    onCellScore(index, score) {
+    onCellScore(index, score, cell) {
       dispatch(Actions.score({ index, score }));
+      if(score > 0) {
+        ga('send', 'event', 'score', 'correct');
+      } else {
+        ga('send', 'event', 'score', 'wrong');
+      }
     },
     onNextPage() {
       dispatch(Actions.buildBoard());
+      ga('send', 'event', 'build_board', 'next_page');
     }
   };
 }
